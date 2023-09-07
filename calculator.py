6e93903379d78aeb61f3394da955ecf60067403e
Hbh_N <meblog@mail.com>
2023-09-07 14:01:59 +1000

    Added multiplication function

--- calculator.py
+++ calculator.py
@@ -1,2 +1,6 @@
 def add_num(a, b):
     return a+b
+
+
+def sub_num(a, b):
+    return a-b
--- test_calc.py
+++ test_calc.py
@@ -9,3 +9,6 @@ class TestCalculator:
 
     def test_addtition(self):
         assert 4 == calculator.add_num(2, 2)
+
+    def test_multiplication(self):
+        assert 1 == calculator.sub_num(3, 2)
